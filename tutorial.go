ad25a14e7fe53e00aa374344bff0b7e88f9ec01b
Wick <wickerdj@gmail.com>
2021-03-14 18:07:18 -0500

    Stuct Methods

--- tutorial.go
+++ tutorial.go
@@ -366,6 +366,49 @@ type Circle2 struct {
 	*Point
 }
 
+func structMethods() {
+	s1 := Student{"Tim", []int{70, 90, 80, 85}, 19}
+	fmt.Println(s1.getAge())
+	s1.setAge(7)
+	fmt.Println(s1)
+	fmt.Println(s1.getAverageGrade())
+	fmt.Println(s1.getMaxGrade())
+}
+
+type Student struct {
+	name   string
+	grades []int
+	age    int
+}
+
+func (s Student) getAge() int {
+	return s.age
+}
+
+func (s *Student) setAge(age int) {
+	s.age = age
+}
+
+func (s Student) getAverageGrade() float32 {
+	sum := 0
+	for _, v := range s.grades {
+		sum += v
+	}
+	return float32(sum) / float32(len(s.grades))
+}
+
+func (s *Student) getMaxGrade() int {
+	curMax := 0
+
+	for _, v := range s.grades {
+		if curMax < v {
+			curMax = v
+		}
+	}
+
+	return curMax
+}
+
 func main() {
 	// calcAge()
 	// aritmetic()
@@ -387,5 +430,6 @@ func main() {
 
 	// mutableAndImmutable()
 	// pointersAndDeference()
-	structsAndCustomTypes()
+	// structsAndCustomTypes()
+	structMethods()
 }
